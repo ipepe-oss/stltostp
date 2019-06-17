bf0929bf55bbe6f568b45e8a08241bf9a86aff11
Patryk Ptasinski <patryk@ipepe.pl>
2019-06-17 14:23:06 +0200

    Added removal of processed files

--- api/index.js
+++ api/index.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-var multer  = require('multer')
-var upload = multer({ dest: '/tmp' })
-const { exec } = require('child_process');
+var express = require('express');
+var cors = require('cors');
+var multer  = require('multer');
+var upload = multer({ dest: '/tmp' });
+var exec = require('child_process').exec;
 
-const app = express();
-const port = process.env.PORT || 5000;
+var app = express();
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(cors());
 
 app.post('/api/process', upload.single('input'), function(req, res) {
-    exec(['/usr/local/bin/stltost', req.file.path, req.file.path + '.stp'].join(' '), (err, stdout, stderr) => {
+    exec(['/usr/local/bin/stltost', req.file.path, req.file.path + '.stp'].join(' '), function(err, stdout, stderr){
+        setTimeout(function(){
+            exec(['rm', req.file.path + '*'].join(' '));
+            console.log('Removing', req.file.path);
+        }, 120*1000); // 120 seconds
         if (err) {
             res.type('application/json');
             res.status(422).send({ error: stdout.replace(req.file.path, req.file.originalname) });
             console.error("Failure in processing", stdout, stderr)
         }else{
-            res.download(req.file.path + '.stp', req.file.originalname + '.stp')
+            res.download(req.file.path + '.stp', req.file.originalname + '.stp');
             console.log("Sucess processing", req.file.originalname)
         }
 
     });
 });
 
-app.listen(port, () => console.log("Listening on port " + port));
+app.listen(port, function(){
+    console.log("Listening on port " + port)
+});
